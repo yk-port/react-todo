048f5d8ef53ba7a37d829f8e31fd25746ba6f5b1
tatsuya <yokokura.tatsuya@gmail.com>
2020-04-17 12:29:40 +0900

    SideBarコンポーネントに分割

--- src/components/App.js
+++ src/components/App.js
@@ -2,64 +2,27 @@ import React, { useState } from 'react';
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import HeaderBar from './HeaderBar'
+import SideBar from './SideBar'
 
 import {
   CssBaseline,
-  Drawer,
-  Hidden,
-  List,
-  ListItem,
-  ListItemText,
-  Toolbar,
-  Typography,
   makeStyles,
-  useTheme
 } from '@material-ui/core';
-
-const drawerWidth = 300;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
   },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
-  appBar: {
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-    },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
-  },
   toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-  },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
   },
-  button: {
-    margin: theme.spacing(1),
-  },
 }));
 
 function App(props) {
-  const { container } = props;
   const classes = useStyles();
-  const theme = useTheme();
-
   const [ todos, setTodos ] = useState([]);
-
   const [ mobileOpen, setMobileOpen ] = useState(false);
 
   const handleDrawerToggle = () => {
@@ -83,61 +46,14 @@ function App(props) {
     setTodos(newTodos);
   }
 
-  const drawer = (
-    <div>
-      <div className={classes.toolbar}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            Group List
-          </Typography>
-        </Toolbar>
-      </div>
-
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map(text => (
-          <ListItem button key={text}>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
   return (
     <div className={classes.root}>
       <CssBaseline />
       <HeaderBar
         handleDrawerToggle={handleDrawerToggle} />
-      <nav className={classes.drawer} aria-label="mailbox folders">
-        <Hidden smUp implementation="css">
-          <Drawer
-            container={container}
-            variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true,
-            }}
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation="css">
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant="permanent"
-            open
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-      </nav>
+      <SideBar
+        mobileOpen={mobileOpen}
+        handleDrawerToggle={handleDrawerToggle} />
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <TodoForm addTodo={addTodo} />
