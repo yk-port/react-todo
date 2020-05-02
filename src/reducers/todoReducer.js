5fbeca10aec3f93f53db97874ad1ea34e93b73cc
tatsuya <yokokura.tatsuya@gmail.com>
2020-05-03 06:19:15 +0900

    fix: シーケンシャルなid番号の付与のロジック変更

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -7,12 +7,9 @@ import {
 const todoReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      const todo = {
-        label: action.label,
-        complete: action.complete
-      }
+      const todo = { label: action.label, complete: action.complete }
       const length = state.length
-      const id = length === 0 ? 1 : state[length].id + 1
+      const id = length === 0 ? 1 : state[length - 1].id + 1
       return [...state, { id, ...todo }]
     case COMPLETE_TODO:
       return state
