aa79a8a45e661a9e7be747916696c3ac19da7363
tatsuya <yokokura.tatsuya@gmail.com>
2020-05-03 07:27:58 +0900

    feat: Context内の処理を移動
    completeTodoとdeleteTodoのメソッドを各コンポーネントに移動

--- src/contexts/TodosContext.js
+++ src/contexts/TodosContext.js
@@ -6,18 +6,6 @@ export const TodosContext = createContext()
 const TodosContextProvider = props => {
   const [ state, dispatch ] = useReducer(todoReducer, [])
 
-  // const removeTodo = index => {
-  //   const newTodos = [...todos];
-  //   newTodos.splice(index, 1);
-  //   setTodos(newTodos);
-  // }
-
-  // const completeTodo = index => {
-  //   const newTodos = [...todos];
-  //   newTodos[index].complete = !newTodos[index].complete;
-  //   setTodos(newTodos);
-  // }
-
   return (
     <TodosContext.Provider value={{ state, dispatch }}>
       {props.children}
